80dd30af505a0c72e013e58a3f836162a6fd44f9
Rumus Bajracharya <rumus.3353@gmail.com>
2022-03-25 12:11:30 +0545

    Routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>Page one</div>;
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page two
-      <button>click me</button>
-    </div>
-  );
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" exact element={<PageOne />}></Route>
-          <Route path="/pagetwo" element={<PageTwo />}></Route>
+          <Route path="/" element={<StreamList />}></Route>
+          <Route path="/streams/new" element={<StreamCreate />}></Route>
+          <Route path="/streams/edit" element={<StreamEdit />}></Route>
+          <Route path="/streams/delete" element={<StreamDelete />}></Route>
+          <Route path="/streams/show" element={<StreamShow />}></Route>
         </Routes>
       </Router>
     </div>
